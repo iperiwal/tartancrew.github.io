e103c0485c15f3ce0a1fa57822c5a89c110a790d
Oscar Bezi <oscar@bezi.io>
2014-06-18 17:05:57 -0400

    Gulpfile now compiles as needed.  Split up data object.

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,107 @@
  */
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var merge = require('merge-stream');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var filesize = require('gulp-filesize');
 
-gulp.task('default', function () {
+var jade = require('gulp-jade');
 
+var uglify = require('gulp-uglify');
+
+var scss = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+
+// set to false to compile for release
+// minifies jade, js, css files
+var dev = true;
+
+// cleans compiled files
+gulp.task('clean', function() {
+    var html = gulp.src('./*.html', {read: false})
+        .pipe(clean());
+    var css = gulp.src('./assets/css/*.css', {read: false})
+        .pipe(clean());
+    var js = gulp.src('./assets/js/*.js', {read: false})
+        .pipe(clean());
+    return merge(html, css, js);
 });
+
+// builds the HTML files
+gulp.task('html', function() {
+    // make configuration object based on whether we're in dev mode
+    var jade_opts = {
+        pretty: dev,
+        compileDebug: dev
+    };
+    return gulp.src('./src/jade/*.jade')
+        .pipe(jade(jade_opts))
+        .pipe(gulp.dest('./'))
+        .pipe(filesize())
+        .on('error', gutil.log);
+});
+
+// builds the JS files
+// minifies and concatenates vendor libraries
+gulp.task('js-vendor', function() {
+    return gulp.src('./src/js/vendor/*.js')
+        .pipe(concat('vendor.js')) // concatenate all the libraries
+        .pipe(gulp.dest('./assets/js')) // output unminified file
+        .pipe(filesize()) // print its size
+        .pipe(uglify()) // minify file
+        .pipe(rename('vendor.min.js')) 
+        .pipe(gulp.dest('./assets/js')) // output minified file
+        .pipe(filesize()) 
+        .on('error', gutil.log); // error handling
+});
+
+gulp.task('js', ['js-vendor'], function() {
+    // only minify if we're not developing
+    if (dev) {
+        return gulp.src('./src/js/*.js')
+            .pipe(gulp.dest('assets/js/')) 
+            .pipe(filesize());
+    } else {
+        return gulp.src('./src/js/*.js')
+            .pipe(uglify())
+            .pipe(rename(function(path) {
+                path.extname = ".min.js"
+            }))
+            .pipe(gulp.dest('assets/js/'))
+            .pipe(filesize()) 
+            .on('error', gutil.log);
+    }
+});
+
+// builds the CSS files and minifies them
+// minifies and concatenates vendor libraries
+gulp.task('css-vendor', function() {
+    return gulp.src('./src/scss/vendor/*.css')
+            .pipe(concat('vendor.css'))
+            .pipe(minifyCSS())
+            .pipe(gulp.dest('./assets/css/vendor.css'))
+            .pipe(filesize())
+            .on('error', gutil.log);
+});
+
+gulp.task('css', ['css-vendor'], function() {
+    if (dev) {
+        return gulp.src('./src/scss/*.scss')
+            .pipe(scss())
+            .pipe(gulp.dest('./assets/css/'))
+            .pipe(filesize())
+            .on('error', gutil.log);
+    } else {
+        return gulp.src('src/scss/*.scss')
+            .pipe(scss())
+            .pipe(minifyCSS())
+            .pipe(gulp.dest('assets/css/'))
+            .pipe(filesize())
+            .on('error', gutil.log);
+    }
+});
+
+gulp.task('default', ['html', 'js', 'css']);
