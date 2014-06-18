47338f0bdc57c883a650b857688ac2b6779efc07
Oscar Bezi <oscar@bezi.io>
2014-06-18 14:48:11 -0400

    Added new files.

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/**
+ * @file gulpfile.js
+ * @brief Builds the TartanCrew website using Gulp
+ * @author Oscar Bezi (bezi@cmu.edu)
+ * @since 17 July 2014
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+
+});
